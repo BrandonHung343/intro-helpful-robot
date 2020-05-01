4bb1259d82d30ac08b1b4f8a3959f2eb6a58e495
Brandon Hung <brandonhung343@gmail.com>
2020-05-01 10:26:15 -0700

    Code

---
+++ controllers/mavic2pro/build/release/mavic2pro.d
@@ -0,0 +1,13 @@
+build/release/mavic2pro.o: mavic2pro.c \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/camera.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/compass.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/gps.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/gyro.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/inertial_unit.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/keyboard.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/led.h \
+ C:\Users\brand\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h
